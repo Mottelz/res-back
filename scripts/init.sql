a82fdb769fcda145acf5e2cfad49b35fb217b8a6
Mottel Zirkind <shachna@gmail.com>
2019-05-29 15:42:42 -0400

    Added a script to initialize the database. (SQLite)

--- scripts/init.sql
+++ scripts/init.sql
@@ -1,48 +1,48 @@
 -- noinspection SqlNoDataSourceInspectionForFile
 
 CREATE TABLE users (
-    uid INT PRIMARY KEY AUTOINCREMENT,
+    uid INTEGER PRIMARY KEY AUTOINCREMENT,
     email VARCHAR UNIQUE NOT NULL,
     password VARCHAR NOT NULL
 );
 
 create table country (
-    cid INT PRIMARY KEY AUTOINCREMENT,
+    cid INTEGER PRIMARY KEY AUTOINCREMENT,
     name VARCHAR NOT NULL UNIQUE,
     code VARCHAR NOT NULL UNIQUE
 );
 
 create table province(
-    pid INT PRIMARY KEY AUTOINCREMENT,
+    pid INTEGER PRIMARY KEY AUTOINCREMENT,
     name VARCHAR UNIQUE NOT NULL,
     code VARCHAR UNIQUE NOT NULL,
-    country INT NOT NULL,
+    country INTEGER NOT NULL,
     foreign key (country) references country(cid)
 );
 
 create table cities(
-    cid INT PRIMARY KEY AUTOINCREMENT,
+    cid INTEGER PRIMARY KEY AUTOINCREMENT,
     name VARCHAR NOT NULL,
-    province INT NOT NULL,
+    province INTEGER NOT NULL,
     foreign key (province) references province(pid)
 );
 
 create table addresses(
-    aid INT PRIMARY KEY AUTOINCREMENT,
+    aid INTEGER PRIMARY KEY AUTOINCREMENT,
     number VARCHAR not null,
     street VARCHAR not null,
     apt VARCHAR,
-    city not null,
+    city integer not null,
     foreign key city references cities(cid)
 );
 
 create table  contact (
-  cid INT PRIMARY KEY AUTOINCREMENT,
-  uid INT UNIQUE NOT NULL,
+  cid INTEGER PRIMARY KEY AUTOINCREMENT,
+  uid INTEGER UNIQUE NOT NULL,
   fname VARCHAR NOT NULL,
   lname VARCHAR NOT NULL,
   email VARCHAR NOT NULL,
-  address INT UNIQUE,
+  address INTEGER UNIQUE,
   website VARCHAR,
   links VARCHAR,
   position VARCHAR,
@@ -51,24 +51,24 @@ create table  contact (
 );
 
 create table skills(
-    sid INT PRIMARY KEY AUTOINCREMENT,
+    sid INTEGER PRIMARY KEY AUTOINCREMENT,
     skill varchar not null,
-    user int not null,
+    user INTEGER not null,
     foreign key (user) references users(uid)
 );
 
 create table projects(
-    pid INT PRIMARY KEY AUTOINCREMENT,
+    pid INTEGER PRIMARY KEY AUTOINCREMENT,
     title varchar not null,
     tech varchar not null,
     description varchar,
-    user int not null,
+    user INTEGER not null,
     foreign key (user) references users(uid)
 );
 
 create table employment(
-    eid INT PRIMARY KEY AUTOINCREMENT,
-    user int not null,
+    eid INTEGER PRIMARY KEY AUTOINCREMENT,
+    user INTEGER not null,
     employer not null,
     start varchar not null,
     end varchar,
@@ -78,8 +78,8 @@ create table employment(
 );
 
 create table education(
-    eid INT PRIMARY KEY AUTOINCREMENT,
-    user int not null,
+    eid INTEGER PRIMARY KEY AUTOINCREMENT,
+    user INTEGER not null,
     level varchar not null,
     school varchar not null,
     completed varchar not null,
